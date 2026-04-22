5f29ff4a5ae794d7de5550e933a156d64c74e8fe
KETK3T <111716884+KETK3T@users.noreply.github.com>
2026-04-22 17:28:55 -0500

    Attempt to fix nav bar block

--- wardrobeapp/App.js
+++ wardrobeapp/App.js
@@ -140,8 +140,8 @@ const styles = StyleSheet.create({
   tabBar: {
     backgroundColor: '#fff',
     borderTopColor: '#eee',
-    height: 70,
-    paddingBottom: 10,
+    height: Platform.OS === 'web' ? 60 : 70,
+    paddingBottom: Platform.OS === 'web' ? 8 : 10,
     paddingTop: 6,
   },
   tabLabel: {
